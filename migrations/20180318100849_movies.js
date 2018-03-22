3f1008dec580c38ebc9d93283f3e70c4a18328f5
Betty Chow <bettychchow@gmail.com>
2018-03-22 14:32:26 -0700

    Add date and time columns to requests table and updated seed file

--- migrations/20180318100849_movies.js
+++ migrations/20180318100849_movies.js
@@ -6,6 +6,7 @@ exports.up = function(knex, Promise) {
     table.string('director').notNullable().defaultTo('')
     table.integer('year').notNullable().defaultTo(0)
     table.integer('rating').notNullable().defaultTo(0)
+    table.string('url').notNullable().defaultTo('')
     table.timestamps(true, true)
   })
 };
--- seeds/movies.js
+++ seeds/movies.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('movies').insert([
-        {id: 1, title: 'Cinema Paradiso', director: 'Giuseppe Tornatore', year: 1988 , rating: 9},
-        {id: 2, title: `It's a Wonderful Life`, director: 'Frank Capra', year: 1946, rating: 8},
-        {id: 3, title: `Schindler's List`, director: 'Steven Spielberg', year: 1993, rating: 9}
+        {id: 1, title: 'Cinema Paradiso', director: 'Giuseppe Tornatore', year: 1988 , rating: 9, url: 'https://images-na.ssl-images-amazon.com/images/I/51S0AP183SL.jpg'},
+        {id: 2, title: `It's a Wonderful Life`, director: 'Frank Capra', year: 1946, rating: 8, url: 'https://images-na.ssl-images-amazon.com/images/I/51IkZQw3MLL.jpg'},
+        {id: 3, title: `Schindler's List`, director: 'Steven Spielberg', year: 1993, rating: 9, url: 'https://images-na.ssl-images-amazon.com/images/I/517nCQA7%2B%2BL.jpg'}
       ]);
     }).then(() => {
       return knex.raw(
